a03bb265a8b5f9b5b976346098406d9fa51f70a1
passwordd009 <elijah.hawes47@gmail.com>
2024-03-19 12:49:55 -0400

    navbar created

---
+++ YouNews/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const NavBar = () => {
+   return(
+    <Navbar expand="xxl" className="bg-body-tertiary" sticky="top" bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home"><h1>YouNews</h1></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="justify-content-end flex-grow-1 pe-3">
+          <Nav.Item>
+             <Nav.Link href="#home"><h4>Home</h4></Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+             <Nav.Link href="#link"><h4>Gaza Info</h4></Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+             <Nav.Link href="#about"><h4>About</h4></Nav.Link>
+          </Nav.Item>
+          </Nav>
+         </Navbar.Collapse>
+      </Container>
+    </Navbar>
+   )
+}
+
+export default NavBar 
