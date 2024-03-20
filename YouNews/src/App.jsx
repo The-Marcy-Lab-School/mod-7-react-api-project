ea3d0246a8d3d957d7c1c899a86040bc5477705e
roenc576 <roenc576@gmail.com>
2024-03-20 16:46:31 -0400

    Some changes

--- YouNews/src/App.jsx
+++ YouNews/src/App.jsx
@@ -1,18 +1,7 @@
-import { useState } from 'react'
-import NavBar from './components/Navbar'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import TrendingArticles from './components/TrendingArticles';
-import TrendingArticleCarousel from './components/TrendingArticleCarousel';
-'../TrendingArticles.css'
-function App() {
 
+function App() {
   return (
-    <>
-     <NavBar> </NavBar>
-     <br></br>
-     <TrendingArticleCarousel></TrendingArticleCarousel>
-    </>
+    
   )
 }
 
