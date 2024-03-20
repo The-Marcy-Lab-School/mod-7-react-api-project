bfb42e0e8108ff40a1e036e71c0c2cc60f81fe40
roenc576 <roenc576@gmail.com>
2024-03-20 16:44:18 -0400

    created carousel component

--- YouNews/src/App.jsx
+++ YouNews/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import NavBar from './components/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-
+import TrendingArticles from './components/TrendingArticles';
+import TrendingArticleCarousel from './components/TrendingArticleCarousel';
+'../TrendingArticles.css'
 function App() {
 
   return (
     <>
      <NavBar> </NavBar>
+     <br></br>
+     <TrendingArticleCarousel></TrendingArticleCarousel>
     </>
   )
 }
--- YouNews/src/Context/TrendingProvider.jsx
+++ YouNews/src/Context/TrendingProvider.jsx
@@ -1,2 +1,32 @@
-import {utils} from '../utils'
+import { useState, useEffect } from 'react';
+import TrendingContext from './TrendingContext';
+import { handleFetch } from '../utils';
 
+const TrendingProvider = ({ children }) => {
+    const [trendingArticles, setTrendingArticles] = useState([]);
+    const [error, setError] = useState(null);
+
+    const url = 'https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?api-key=BzrIgxaSecJBuUXeSAMRvrsAes0xrlrU'
+
+    useEffect(() => {
+        const doFetch = async () => {
+            const [data, error] = await handleFetch(url);
+            if (data) setTrendingArticles(data.results);
+            if (error) setError(error);
+            console.log(data.results);
+        }
+        doFetch();
+    }, []);
+
+    let contextValues = { trendingArticles, error };
+
+    return (
+        <TrendingContext.Provider value={contextValues}>
+            { children }
+        </TrendingContext.Provider>
+    )
+
+
+}
+
+export default TrendingProvider;
