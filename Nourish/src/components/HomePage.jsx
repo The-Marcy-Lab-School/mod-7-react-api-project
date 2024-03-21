bac7f29eb547f93f63649e3279b92a62149d8819
Tahj <tahjamie1@gmail.com>
2024-03-21 14:19:22 -0400

    Updates Routing etc

---
+++ Nourish/src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Noursih</h1>
+      <h2>Wellness starts here..</h2>
+    </div>
+  )
+}
+
+export default HomePage
