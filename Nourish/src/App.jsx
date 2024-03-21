8578dd9b1ab8b6c55f02393e5ea297bfbf4407d0
Your Name <domtuz@yahoo.com>
2024-03-21 14:22:19 -0400

    Working on the Navbar

--- Nourish/src/App.jsx
+++ Nourish/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import { Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-    //*routes
+
+    // *routes
 
     </>
   )
---
+++ Nourish/src/components/NavBar.jsx
@@ -0,0 +1,6 @@
+
+const NavBar = () => {
+    
+}
+
+export default NavBar
