4c73a0f40dc1af6e4136e4e5548bbe8ef92acae3
jahmarimaxwell <jahmari.maxwell@gmail.com>
2024-08-26 18:11:47 -0400

    inlcuding in the about page

--- marvel-comics/src/pages/About.jsx
+++ marvel-comics/src/pages/About.jsx
@@ -1,3 +1,11 @@
-const About = () => <h1>About</h1>;
-
+const About = () => {
+    <>
+    <h1>About</h1>
+    <p> Dive into the Marvel Universe with ease using our API searcher app. 
+        Powered by the Marvel API, Marvel Museum lets you effortlessly 
+        search for your favorite characters. Upon user input our website will fetch stunning thumbnails, 
+        character names, character descriptions all in one place. 
+        Explore the heroes, villains, and everything in between—right from the heart of the Marvel cosmos!</p>
+    </>;
+}
 export default About;
