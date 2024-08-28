8e8b888bd0b46c34622e33db41e4a903ec0b7e1f
WilliamZeng <itswilliamzeng@gmail.com>
2024-08-27 20:07:38 -0400

    fetch-events

--- marvel-comics/src/App.jsx
+++ marvel-comics/src/App.jsx
@@ -9,12 +9,15 @@ import MarvelGallery from './pages/MarvelGallery'
 import './App.css';
 ///////
 import MarvelSearch from './components/MarvelSearch'; // Ensure this component is correctly imported
+import Events from './pages/Events'
 
 // Create the context
 const CharacterContext = createContext();
 const ComicContext = createContext();
+const EventContext = createContext();
 
 const App = () => {
+  const [events, setEvents] = useState([]);
   const [comics, setComics] = useState([]);
   const [characters, setCharacters] = useState([]); // Change to an array to hold multiple characters
   const [error, setError] = useState('');
@@ -23,17 +26,23 @@ const App = () => {
     <BrowserRouter>
       <CharacterContext.Provider value={{ characters, error, setCharacters, setError }}>
 
-        <ComicContext.Provider value={{ comics, setComics, setError }}>
-          <div className="main-content">
-            <NavBar />
-            {/* Using a nav bar in react */}
-            {/* "/" is home page */}
-            <Routes >
-              <Route path="/pages/about" element={<About />}> </Route>
-              <Route path="/" element={<MarvelSearch />}> </Route>
-              <Route path="/pages/MarvelGallery" element={<MarvelGallery />}> </Route>
-            </Routes>
-          </div>
+        <ComicContext.Provider value={{ comics, error, setComics, setError }}>
+          <EventContext.Provider value={{ events, error, setEvents, setError }}>
+            {/* <EventContext.Provider value={{ events, error, setEvents, setError }}> */}
+            <div className="main-content">
+              <NavBar />
+              {/* Using a nav bar in react */}
+              {/* "/" is home page */}
+              <Routes >
+                <Route path="/pages/about" element={<About />}> </Route>
+                <Route path="/" element={<MarvelSearch />}> </Route>
+                {/* <Route path="/" element={<EventDisplay />}> </Route> */}
+                <Route path="/events/:characterId" element={<Events />} ></Route>
+                <Route path="/pages/MarvelGallery" element={<MarvelGallery />}> </Route>
+              </Routes>
+            </div>
+            {/* </EventContext.Provider> */}
+          </EventContext.Provider>
         </ComicContext.Provider>
 
       </CharacterContext.Provider>
@@ -44,7 +53,9 @@ const App = () => {
 // Create a custom hook to use the CharacterContext
 const useCharacter = () => useContext(CharacterContext);
 const useComic = () => useContext(ComicContext)
+const useEvent = () => useContext(EventContext)
 
 export default App;
 export { useCharacter }
 export { useComic }
+export { useEvent }
---
+++ marvel-comics/src/components/EventDisplay.jsx
@@ -0,0 +1,26 @@
+import { useEvent } from '../App'; // Import the custom hook to access context
+
+
+const EventDisplay = () => {
+  const { events } = useEvent(); // Get the characters from context
+
+  console.log(events)
+
+  return (
+    <div className="event-display">
+      {events.map((event) => (
+        <div>
+          {/* className="event-card" key={event.id}> */}
+          <h2 className="marvel-event-title">{event.title}</h2>
+          {/* <img
+            className="event-image"
+            src={`${event.thumbnail.path}.${event.thumbnail.extension}`} //extension ensures type is jpg,etc avoiding errors
+            alt={event.name} */}
+          {/* /> */}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default EventDisplay;
